0748f2eef071db85e97e75c3f0d5887d45d13055
Astaarr <7dcv2005@gmail.com>
2025-02-07 10:54:54 +0100

    Funcionalidad guardado de datos Formulario

---
+++ app/backend/reserva/reservaPadre.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+// Habilitar errores
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// CORS
+header("Access-Control-Allow-Origin: *"); // Permite peticiones desde cualquier dominio
+header("Access-Control-Allow-Methods: POST, OPTIONS"); // Permite POST y la pre-flight request (OPTIONS)
+header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Permite enviar datos JSON y tokens
+
+include '../conecta.php';
+
+
+$datosTutor = json_decode(file_get_contents('php://input'), true);
+
+
+?>
